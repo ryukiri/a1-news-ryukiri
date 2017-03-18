f7f479f9902040d04cafd9772814460a7b68d613
Michael Freeman <michael.k.freeman@gmail.com>
2017-03-18 09:01:45 -0700

    Added README and session files

---
+++ README.md
@@ -0,0 +1,5 @@
+# a1-news
+
+In this assignment, you'll edit this `README.md` file to create a Markdown document that describes a news story (full instructions on Canvas). You'll also keep track of the terminal commands you run in the `session.sh` file (you don't ever need to run that file).
+
+When you're finished, please **delete this text**, leaving only the Markdown that you've written. Make sure to **submit this GitHub URL** to Canvas on time for full credit.
---
+++ session.sh
@@ -0,0 +1,28 @@
+# session.sh
+# Track the commands that you execute in your terminal below
+
+# Print your working directory
+
+
+# Change your directory to a folder in which you do work for this class
+# Make sure to use the "~" shortcut rather than specifying your username
+
+
+# Clone your private assignment repository from GitHub to your machine
+
+
+# Change your directory to inside of your "a1-news-USERNAME" folder
+
+
+# Make a new folder called "imgs" - you'll download an image into this folder
+
+
+# At appropriate checkpoints, you'll need to do the following:
+# Add all of your changes that you've made to git
+
+
+# Make a commit, including a descriptive message
+
+
+# Push your change up to GitHub
+
